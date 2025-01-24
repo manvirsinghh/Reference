ff794c070b9875588690daf08b2472777ddc2ab9
Manvir Singh <147043473+manvirsinghh@users.noreply.github.com>
2025-01-24 10:49:39 -0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-// Example JavaScript for form submission alert
+
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
     alert("Thank you for contacting me! I'll get back to you soon.");
