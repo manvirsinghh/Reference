6f11928e3a091bda32df6b1920bb7bdc9e85f517
manvirsinghh <manvirsingh6656@gmail.com>
2025-01-24 09:39:05 -0700

    website to app

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+// Example JavaScript for form submission alert
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("Thank you for contacting me! I'll get back to you soon.");
+  });
+  
---
+++ sw.js
@@ -0,0 +1,43 @@
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/javascript/script.js",
+  "/icons/android-chrome-192x192.png", 
+  "/icons/android-chrome-512x512.png" 
+];
+
+// Install the service worker and cache important files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// Activate the service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
